c32de3b10552d9a9025c207eed09ff675e5bdde5
tdmpajak <tdm.pajak10@gmail.com>
2026-07-24 14:37:37 +0700

    Add NC_VERIFIER_URL to config.js
    Added NC_VERIFIER_URL for redirecting access to Tax.

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,8 @@
 // GANTI dengan URL Web App hasil Deploy Google Apps Script Anda
 // Contoh: https://script.google.com/macros/s/AKfycb.../exec
 const API_URL = "https://script.google.com/macros/s/AKfycbzobKrrnIIktNv1wLoqp6DopSYQM3ua581U7esOvoD-Vl43awmze0tHegzh7DTe6jRUYQ/exec";
+
+// URL situs NC Verifier (repo terpisah) -- dipakai oleh index.html (portal)
+// untuk mengarahkan "Akses Tax" setelah kode akses benar. Cek dulu URL GitHub
+// Pages NC Verifier Anda yang sebenarnya sebelum deploy (Settings > Pages).
+const NC_VERIFIER_URL = "https://tdmpajak.github.io/NC-Verifier-Tax/";
